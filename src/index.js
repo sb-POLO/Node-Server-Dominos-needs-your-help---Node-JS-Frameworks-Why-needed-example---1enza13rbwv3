d0791bb3d9782269a10e92cb9cb983b678d9d71f
Sourav Banerjee <rjsourav88@gmail.com>
2022-09-16 17:24:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+  if(req.url === '/welcome'){
+    res.writeHead(200,{
+      'Content-Type': 'text/plain'
+    });
+    res.end('Welcome to Dominos!');
+  }
+  if(res.url === '/contact'){
+    res.writeHead(200,{
+      'Content-Type': 'application/json';
+    });
+    res.end({
+       phone: '18602100000',
+       email: 'guestcaredominos@jublfood.com'
+    });
+  }
 }
+
+httpServer.listen(8081);
 
 module.exports = httpServer;
